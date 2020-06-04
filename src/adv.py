8bf0300c3a0e1fa09f30c881c9ea2cd15b909f8a
Michael Bidnyk <mbidnyk1@gmail.com>
2020-06-03 19:23:28 -0500

    added player, room, and item classes, along with repl movement

--- src/adv.py
+++ src/adv.py
@@ -1,11 +1,14 @@
+import textwrap
 from room import Room
+from player import Player
+from item import Item
 
+wrapper = textwrap.TextWrapper(width=50)
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
-
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
@@ -21,6 +24,12 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+items = [
+    Item("Salamandra", "A fiery sword deadly to all dinosaurs."),
+    Item("Black_Pendant", "An ominous glow surrounds this ancient pendant."),
+    Item("Brutal_Potion", "Just a drop of this potion can infuse you with maddening strength."),
+    Item("Butterfly_Dagger", "An extremely light dagger, with Elma engraved at the hilt."),
+]
 
 # Link rooms together
 
@@ -49,3 +58,59 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+room['outside'].add_items(items[0])
+room['foyer'].add_items(items[1])
+player_name = input("What is your name? \n")
+player = Player(player_name,"Outside Cave Entrance")
+curr_room = room['outside']
+action_error = 'Movement not allowed in that direction. **Use keys: [n, s, e, w] for each respective direction**'
+
+while True:
+    print(f'You arrive at the {player.current_room}')
+    wrap_descrip = wrapper.wrap(text=curr_room.description)
+    print(f'{wrap_descrip}')
+    print(f'{curr_room.loot}')
+    action = input('You can move north, south, east, or west. **Use keys: [n, s, e, w] for each respective direction** \n')
+    if action == 'n':
+        try: 
+            curr_room.n_to
+        except AttributeError: 
+            print(action_error)
+        else:
+            curr_room = curr_room.n_to
+            player.current_room = curr_room.name
+            print(f'You move north')
+    elif action == 's':
+        try: 
+            curr_room.s_to
+        except AttributeError: 
+            print(action_error)
+        else:
+            curr_room = curr_room.s_to
+            player.current_room = curr_room.name
+            print(f'You move south')
+    elif action == 'e':
+        try: 
+            curr_room.e_to
+        except AttributeError: 
+            print(action_error)
+        else:
+            curr_room = curr_room.e_to
+            player.current_room = curr_room.name
+            print(f'You move east')
+    elif action == 'w':
+        try: 
+            curr_room.w_to
+        except AttributeError: 
+            print(action_error)
+        else:
+            curr_room = curr_room.w_to
+            player.current_room = curr_room.name
+            print(f'You move west')
+    elif action == 'q':
+        exit()
+    else:
+        print(action_error)
+    
+
+
--- src/player.py
+++ src/player.py
@@ -1,2 +1,21 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+        self.inventory = []
+    def add_items(self,item):
+        self.inventory.append(item)
+    def __str__(self):
+        return f"Player: {self.name}"
+    def __repr__(self):
+        return f"Player({repr(self.name)})"
+
+# class Inventory(Player):
+#     def __init__(self, items):
+#         self.items = items
+#     def __str__(self):
+#         return f"Inventory: {self.items}"
+#     def __repr__(self):
+#         return f"Inventory({repr(self.items)})"
